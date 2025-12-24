06a6495255cf42082d60c998928ee2102e48704a
Phuvm <Phuvm1288@gmail.com>
2025-12-24 10:12:05 +0700

    chore: remove build artifacts from repo

--- TAS-master/Models/UserAccount.cs
+++ TAS-master/Models/UserAccount.cs
@@ -88,24 +88,24 @@ namespace TAS.Models
 	{
 		public int Id { get; set; }
 		public int rowNo { get; set; }
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
 		public bool IsActive { get; set; }
 		public DateTime? CreatedAtUtc { get; set; }
-		public string CreatedBy { get; set; }
+		public string? CreatedBy { get; set; }
 		public DateTime? UpdatedAtUtc { get; set; }
-		public string UpdatedBy { get; set; }
+		public string? UpdatedBy { get; set; }
 		public DateTime? LoginUtc { get; set; }
 		public DateTime? LogOutUtc { get; set; }
-		public string UserName { get; set; }
-		public string NormalizedUserName { get; set; }
-		public string Email { get; set; }
-		public string NormalizedEmail { get; set; }
+		public string? UserName { get; set; }
+		public string? NormalizedUserName { get; set; }
+		public string? Email { get; set; }
+		public string? NormalizedEmail { get; set; }
 		public bool EmailConfirmed { get; set; }
-		public string PasswordHash { get; set; }
-		public string SecurityStamp { get; set; }
-		public string ConcurrencyStamp { get; set; }
-		public string PhoneNumber { get; set; }
+		public string? PasswordHash { get; set; }
+		public string? SecurityStamp { get; set; }
+		public string? ConcurrencyStamp { get; set; }
+		public string? PhoneNumber { get; set; }
 		public bool PhoneNumberConfirmed { get; set; }
 		public bool TwoFactorEnabled { get; set; }
 		public DateTimeOffset? LockoutEnd { get; set; }
